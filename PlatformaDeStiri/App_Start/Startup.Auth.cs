6a7e472391629a3adc5ce2e10d5c18a05feb486f
andrei2d <andrei.tudorache100@gmail.com>
2019-12-06 16:32:36 +0200

    Added user roles

--- PlatformaDeStiri/App_Start/Startup.Auth.cs
+++ PlatformaDeStiri/App_Start/Startup.Auth.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
@@ -17,6 +18,7 @@ namespace PlatformaDeStiri
             // Configure the db context, user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
@@ -64,5 +66,42 @@ namespace PlatformaDeStiri
             //    ClientSecret = ""
             //});
         }
+
+        private void createAdminUserAndApplicationRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            // Se adauga rolurile aplicatiei
+            if (!roleManager.RoleExists("Administrator"))
+            {
+                // Se adauga rolul de administrator
+                var role = new IdentityRole();
+                role.Name = "Administrator";
+                roleManager.Create(role);
+                // se adauga utilizatorul administrator
+                var user = new ApplicationUser();
+                user.UserName = "admin@admin.com";
+                user.Email = "admin@admin.com";
+                var adminCreated = UserManager.Create(user, "Administrator1!");
+                if (adminCreated.Succeeded)
+                {
+                    UserManager.AddToRole(user.Id, "Administrator");
+                }
+            }
+            if (!roleManager.RoleExists("Editor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Editor";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+        }
+
     }
 }
--- PlatformaDeStiri/Startup.cs
+++ PlatformaDeStiri/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using PlatformaDeStiri.Models;
 
 [assembly: OwinStartupAttribute(typeof(PlatformaDeStiri.Startup))]
 namespace PlatformaDeStiri
@@ -9,6 +10,7 @@ namespace PlatformaDeStiri
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createAdminUserAndApplicationRoles();
         }
     }
 }
